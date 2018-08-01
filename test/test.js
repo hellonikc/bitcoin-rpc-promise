16d7c857e0cf69665444f1ae51f1deb6ca5fe8d4
onetomonejerry <hellonikc@outlook.com>
2018-08-01 05:29:15 +0000

    add methods for v0.16

--- test/test.js
+++ test/test.js
@@ -4,10 +4,13 @@ let rpc = new BitcoinRpc('http://user:password@localhost:8332');
 
 describe('bitcoin rpc', () => {
   it('should call succesfully', (done) => {
-    rpc.call('getblockcount').then(result => done());
+    rpc.call('getblockhash', 0).then(result => done());
   });
   it('should call wrapper method succesfully', (done) => {
     rpc.getBlockHash(0).then(result => done());
+  });
+  it('should call lower case wrapper method succesfully', (done) => {
+    rpc.getblockhash(0).then(result => done());
   });
   it('should throw error for invalid command', (done) => {
     rpc.call('invalid_command').catch(error => done());
